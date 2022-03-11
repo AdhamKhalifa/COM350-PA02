d6bdd785e425be638e2e687b398565165d9efa15
AlexiaBalentine <abalentine8@gmail.com>
2022-03-11 17:21:25 -0500

    Test
    Test

---
+++ app/src/main/java/com/example/booklist/MainActivity.java
@@ -0,0 +1,16 @@
+// PA02 by Adham and Alexia
+
+package com.example.booklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
